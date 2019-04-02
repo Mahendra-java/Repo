3eabc353e90928014f50bcb041d6da2d7e157f43
DEV01 <mahendra@mahendra-PC>
2019-04-02 22:14:10 +0530

    PLEASE CHECK ERROR

---
+++ Proj01/src/com/app/Project01.java
@@ -0,0 +1,17 @@
+package com.app;
+
+public class Project01 {
+	int x=10;
+	void m1(int i) {
+		i=x;
+		System.out.println(i);
+	}
+	
+	public static void main(String[] args) {
+		Project01 p=new Project01();
+		System.out.println("Hello Git");
+		p.m1(22);
+		Project02=new project02();
+	}
+
+}
